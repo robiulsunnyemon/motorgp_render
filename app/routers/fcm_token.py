fb13c69a86b0b876cb251279d0df5a1d80a5450f
robiulsunnyemon <robiulsunyemon@gmail.com>
2025-09-24 15:53:16 +0600

    update fcm token

--- app/routers/fcm_token.py
+++ app/routers/fcm_token.py
@@ -5,11 +5,17 @@ from app.models.fcm_token import FCMTokenModel
 from app.schemas.fcm_token import FCMToken
 from typing import List
 
+from app.utils.user_info import get_user_info
+
 fcm_token_router = APIRouter(prefix="/fcm_token", tags=["FCM Token"])
 
 @fcm_token_router.post('/', response_model=FCMToken, status_code=status.HTTP_201_CREATED)
-async def create_token(token: FCMToken, db: Session = Depends(get_db)):
-    new_token = FCMTokenModel(**token.model_dump())
+async def create_token(token: FCMToken, db: Session = Depends(get_db),user: dict = Depends(get_user_info)):
+    user_id = user["user_id"]
+    new_token = FCMTokenModel(
+        user_id=user_id,
+        token=token.token
+    )
     db.add(new_token)
     db.commit()
     db.refresh(new_token)
